48e215647888b112f0caf0c549cd47f18625bbe0
Guruprakash Prakash <54737169+7708653907@users.noreply.github.com>
2020-02-04 13:42:08 +0530

    odd.js

---
+++ js/.oddjs.js
@@ -0,0 +1,11 @@
+function areBothOdd(num1, num2){
+    if((num1%2== 0) && (num2%2== 0))
+    alert(true)
+  else     
+   {
+     alert(false)
+   }
+}    
+num1=(prompt("enter value"))
+num2=(prompt("enter value"))
+areBothOdd(num1,num2)

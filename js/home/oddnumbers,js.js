847a6262859e2ecaea206836ddcb17e82ad022ec
Guruprakash Prakash <54737169+7708653907@users.noreply.github.com>
2020-02-05 19:05:46 +0530

    Add files via upload

---
+++ js/home/oddnumbers,js.js
@@ -0,0 +1,13 @@
+  var odd=function(a)
+    {
+       for(i=0;i<length;i++);
+       {
+           if(a[1]%2==0)
+           {
+              console.log(a); 
+
+           }
+       }
+    }
+array=[1,2,3,4,5]
+odd=array   
